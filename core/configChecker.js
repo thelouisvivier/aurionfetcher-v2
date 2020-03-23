c9ffd5174b64be8175fddd7f8ee2ffea984cb401
Louis <22093201+thelouisvivier@users.noreply.github.com>
2020-03-23 16:45:46 +0100

    Config file checker

--- core/configChecker.js
+++ core/configChecker.js
@@ -1,8 +1,21 @@
 const config = require('../config/config.json')
 
 module.exports = async function(trail){
-
-
+    if (config.username == ""){
+        throw new Error("config.json : Empty username");
+    }
+    if (config.password == ""){
+        throw new Error("config.json : Empty password");
+    }
+    if (config.domain == ""){
+        throw new Error("config.json : Empty domain");
+    }
+    if (config.telegramBotToken == ""){
+        throw new Error("config.json : Empty telegramBotToken");
+    }
+    if (config.telegramChatId == ""){
+        throw new Error("config.json : Empty telegramChatId");
+    }
     console.log("Config file checked");
     return true;
 };
